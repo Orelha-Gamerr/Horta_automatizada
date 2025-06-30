6d3cb8587e56ffcd000425efdbd8f1ec06948492
Orelha-Gamerr <angelolucietto@gmail.com>
2025-06-30 15:03:51 -0300

    Add timezone setting for São Paulo in insert.php

--- insert.php
+++ insert.php
@@ -2,6 +2,7 @@
 include_once 'db_class.php';
 $db = new db("medicao");
 
+date_default_timezone_set('America/Sao_Paulo'); // Define o fuso horário para São Paulo
 $dados = [
     'umidade' => $_POST['umidade'] ?? '0',
     'data' => date('Y-m-d H:i:s')
